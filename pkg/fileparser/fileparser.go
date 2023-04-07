a63026d9e100b5d180cf0f79db838ce5be5c04a1
Luis.Fernando <luisfmiranda8@gmail.com>
2023-04-07 17:23:05 -0500

    refactoring in small functions

--- pkg/fileparser/fileparser.go
+++ pkg/fileparser/fileparser.go
@@ -6,7 +6,6 @@ import (
 	"os"
 	"regexp"
 	"strings"
-	"sync"
 )
 
 func GetFileExtension(filename string) string {
@@ -25,85 +24,85 @@ func IsTitle(line string) bool {
 // and returns an array of strings containing the chunks.
 // chunkSize is the maximum number of words per chunk, and overlap is the number of chunks
 // to overlap between consecutive chunks.
-func ParseTxtInChunks(filePath string, chunkSize int, overlap int) ([]string, error) {
+func ReadWords(filePath string, wordChan chan<- string, errChan chan<- error) {
 	file, err := os.Open(filePath)
 	if err != nil {
-		return nil, err
+		errChan <- err
+		return
 	}
 	defer file.Close()
 
-	var chunks []string
-	var words []string
+	reader := bufio.NewReader(file)
 	var currentWord strings.Builder
 
-	reader := bufio.NewReader(file)
-	wordChan := make(chan string)
-	done := make(chan struct{})
-	errChan := make(chan error)
+	for {
+		b, err := reader.ReadByte()
 
-	go func() {
-		for {
-			b, err := reader.ReadByte()
-
-			if err == nil || err == io.EOF {
-				if b == ' ' || b == '\n' {
-					word := currentWord.String()
-					if len(word) > 0 {
-						wordChan <- word
-					}
-					currentWord.Reset()
-				} else {
-					currentWord.WriteByte(b)
+		if err == nil || err == io.EOF {
+			if b == ' ' || b == '\n' {
+				word := currentWord.String()
+				if len(word) > 0 {
+					wordChan <- word
 				}
+				currentWord.Reset()
+			} else {
+				currentWord.WriteByte(b)
+			}
 
-				if err == io.EOF {
-					close(wordChan)
-					return
-				}
-			} else if err != nil {
-				errChan <- err
+			if err == io.EOF {
+				close(wordChan)
 				return
 			}
+		} else if err != nil {
+			errChan <- err
+			return
 		}
-	}()
+	}
+}
 
-	wg := sync.WaitGroup{}
-	wg.Add(1)
+func ParseWordsInChunks(wordChan <-chan string, chunkSize int, overlap int, chunksChan chan<- string, errChan chan<- error) {
+	var words []string
 
-	go func() {
-		defer wg.Done()
-		for {
-			select {
-			case word, ok := <-wordChan:
-				if !ok {
-					done <- struct{}{}
-					return
-				}
-				words = append(words, word)
+	for word := range wordChan {
+		words = append(words, word)
 
-				if len(words) >= chunkSize {
-					chunk := strings.Replace(strings.Join(words[:chunkSize], " "), "\n", " ", -1)
-					chunk = strings.Replace(chunk, "\"", "''", -1)
-					chunks = append(chunks, chunk)
+		if len(words) >= chunkSize {
+			chunk := strings.Replace(strings.Join(words[:chunkSize], " "), "\n", " ", -1)
+			chunk = strings.Replace(chunk, "\"", "''", -1)
+			chunksChan <- chunk
 
-					words = words[chunkSize-overlap:]
-				}
-			case err := <-errChan:
-				done <- struct{}{}
-				errChan <- err
-				return
-			}
+			words = words[chunkSize-overlap:]
 		}
-	}()
-
-	<-done
-	wg.Wait()
+	}
 
 	if len(words) > 0 {
 		chunk := strings.Replace(strings.Join(words, " "), "\n", " ", -1)
 		chunk = strings.Replace(chunk, "\"", "''", -1)
-		chunks = append(chunks, chunk)
+		chunksChan <- chunk
 	}
+
+	close(chunksChan)
+}
+
+func ParseTxtInChunks(filePath string, chunkSize int, overlap int) ([]string, error) {
+	wordChan := make(chan string)
+	chunksChan := make(chan string)
+	errChan := make(chan error)
+
+	go ReadWords(filePath, wordChan, errChan)
+	go ParseWordsInChunks(wordChan, chunkSize, overlap, chunksChan, errChan)
+
+	var chunks []string
+	done := make(chan struct{})
+
+	go func() {
+		for chunk := range chunksChan {
+			chunks = append(chunks, chunk)
+		}
+		done <- struct{}{}
+	}()
+
+	<-done
 
 	select {
 	case err := <-errChan:
