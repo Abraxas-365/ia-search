2e2be211feb33169e28baaa615c1d00323bb7dc1
Luis.Fernando <luisfmiranda8@gmail.com>
2023-04-07 13:46:04 -0500

    new version , now using chunks instead of paragraphs

--- pkg/fileparser/fileparser.go
+++ pkg/fileparser/fileparser.go
@@ -2,22 +2,16 @@ package fileparser
 
 import (
 	"bufio"
-	"errors"
-	"io/ioutil"
+	"io"
 	"os"
 	"regexp"
 	"strings"
-
-	"github.com/unidoc/unioffice/document"
-	"github.com/yuin/goldmark"
-	"github.com/yuin/goldmark/ast"
-	"github.com/yuin/goldmark/text"
 )
 
-func getFileExtension(filename string) string {
+func GetFileExtension(filename string) string {
 	return strings.ToLower(filename[strings.LastIndex(filename, "."):])
 }
-func isTitle(line string) bool {
+func IsTitle(line string) bool {
 	titleRegex := `^[A-Z][a-zA-Z]*( [A-Z][a-zA-Z]+)*\.?$`
 	match, err := regexp.MatchString(titleRegex, line)
 	if err != nil {
@@ -26,104 +20,63 @@ func isTitle(line string) bool {
 	return match
 }
 
-func parseMarkdown(filepath string) ([]string, error) {
-	data, err := ioutil.ReadFile(filepath)
-	if err != nil {
-		return nil, err
-	}
-
-	md := goldmark.New(goldmark.WithExtensions())
-	parsed := md.Parser().Parse(text.NewReader(data))
-
-	var paragraphs []string
-	ast.Walk(parsed, func(n ast.Node, entering bool) (ast.WalkStatus, error) {
-		if entering {
-			if para, ok := n.(*ast.Paragraph); ok {
-				segment := para.Lines().At(0)
-				text := strings.TrimSpace(string(data[segment.Start:segment.Stop]))
-				if text != "" {
-					paragraphs = append(paragraphs, text)
-				}
-			}
-		}
-		return ast.WalkContinue, nil
-	})
-
-	return paragraphs, nil
-}
-
-func parseDoc(filepath string) ([]string, error) {
-	doc, err := document.Open(filepath)
-	if err != nil {
-		return nil, err
-	}
-
-	var paragraphs []string
-	for _, para := range doc.Paragraphs() {
-		paraContent := ""
-		for _, run := range para.Runs() {
-			paraContent += run.Text()
-		}
-		paraContent = strings.TrimSpace(paraContent)
-		if paraContent != "" && !isTitle(paraContent) {
-			paragraphs = append(paragraphs, paraContent)
-		}
-	}
-	return paragraphs, nil
-}
-
-func parseTxt(filepath string) ([]string, error) {
-	file, err := os.Open(filepath)
+// ReadFileInChunks reads a file and separates it into chunks of the specified size,
+// and returns an array of strings containing the chunks.
+// chunkSize is the maximum number of words per chunk, and overlap is the number of chunks
+// to overlap between consecutive chunks.
+func ParseTxtInChunks(filePath string, chunkSize int, overlap int) ([]string, error) {
+	file, err := os.Open(filePath)
 	if err != nil {
 		return nil, err
 	}
 	defer file.Close()
 
-	var paragraphs []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := strings.TrimSpace(scanner.Text())
-		if line != "" && !isTitle(line) {
-			paragraphs = append(paragraphs, line)
+	var chunks []string
+	var words []string
+	var currentWord strings.Builder
+
+	reader := bufio.NewReader(file)
+
+	for {
+		b, err := reader.ReadByte()
+
+		if err == nil || err == io.EOF {
+			if b == ' ' || b == '\n' {
+				word := currentWord.String()
+				if len(word) > 0 {
+					words = append(words, word)
+				}
+				currentWord.Reset()
+			} else {
+				currentWord.WriteByte(b)
+			}
+
+			// If EOF reached, break the loop after processing the last word
+			if err == io.EOF {
+				break
+			}
+		} else if err != nil {
+			return nil, err
+		}
+
+		// Check if we have enough words for a chunk
+		if len(words) >= chunkSize {
+			// Replace newline characters with spaces and replace double quotes with two single quotes
+			chunk := strings.Replace(strings.Join(words[:chunkSize], " "), "\n", " ", -1)
+			chunk = strings.Replace(chunk, "\"", "''", -1)
+			chunks = append(chunks, chunk)
+
+			// Remove the first "chunkSize - overlap" words
+			words = words[chunkSize-overlap:]
 		}
 	}
 
-	if err := scanner.Err(); err != nil {
-		return nil, err
-	}
-	return paragraphs, nil
-}
-
-func parseWrapper(filepath string, parseFunc func(string) ([]string, error), resultChan chan []string, errChan chan error) {
-	paragraphs, err := parseFunc(filepath)
-	if err != nil {
-		errChan <- err
-	} else {
-		resultChan <- paragraphs
-	}
-}
-
-// ParseFile takes a file path as an argument and returns an array of parsed paragraphs.
-
-func ParseFile(filepath string) ([]string, error) {
-	resultChan := make(chan []string)
-	errChan := make(chan error)
-
-	switch ext := getFileExtension(filepath); ext {
-	case ".md":
-		go parseWrapper(filepath, parseMarkdown, resultChan, errChan)
-	case ".doc", ".docx":
-		go parseWrapper(filepath, parseDoc, resultChan, errChan)
-	case ".txt":
-		go parseWrapper(filepath, parseTxt, resultChan, errChan)
-	default:
-		return nil, errors.New("unsupported file type")
+	// Add the remaining words as the last chunk
+	if len(words) > 0 {
+		chunk := strings.Replace(strings.Join(words, " "), "\n", " ", -1)
+		chunk = strings.Replace(chunk, "\"", "''", -1)
+		chunks = append(chunks, chunk)
 	}
 
-	select {
-	case paragraphs := <-resultChan:
-		return paragraphs, nil
-	case err := <-errChan:
-		return nil, err
-	}
+	return chunks, nil
 }
